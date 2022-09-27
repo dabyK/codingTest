471db92a6123312aa85ee163420619b3e27a840c
dabyK <ta0319@naver.com>
2022-09-27 23:08:40 +0900

    #이것이취업을위한코딩테스트다 - 3장 그리디 > 숫자카드게임
    또 다른 풀이 추가

--- codingTest/src/dongBin/Greedy2.java
+++ codingTest/src/dongBin/Greedy2.java
@@ -35,8 +35,20 @@ public class Greedy2 {
 			result = result > min ? result : min;
 		}
 		
-		System.out.println(result);
+		System.out.print(result);
 		
+		//또 다른 방식
+//        for (int i = 0; i < n; i++) {
+//            // 현재 줄에서 '가장 작은 수' 찾기
+//            int min_value = 10001;
+//            for (int j = 0; j < m; j++) {
+//                int x = sc.nextInt();
+//                min_value = Math.min(min_value, x);
+//            }
+//            // '가장 작은 수'들 중에서 가장 큰 수 찾기
+//            result = Math.max(result, min_value);
+//        }
+    
 	}
 	
 	
